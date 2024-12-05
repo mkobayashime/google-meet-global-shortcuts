9e569bb91f8cd7f9c044bb9a026120601bb29c83
Masaki Kobayashi <contact@mkobayashi.me>
2024-12-05 14:40:36 +0900

    Enable to set a shortcut for activating Meet tab

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,37 +1,49 @@
+const findMeetTab = async () => {
+  const tabIDs = (
+    await chrome.tabs.query({
+      url: "https://meet.google.com/*",
+    })
+  ).flatMap(({ id }) => (id === undefined ? [] : [id]));
+
+  if (tabIDs.length === 0) {
+    return;
+  }
+
+  if (tabIDs.length > 1) {
+    chrome.notifications.create({
+      title: chrome.runtime.getManifest().name,
+      message:
+        "Error: Multiple tabs of Google Meet exist. Please close unnecessary ones and keep one.",
+      type: "basic",
+      iconUrl: "icon/128.png",
+    });
+
+    return;
+  }
+
+  return tabIDs[0];
+};
+
 export default defineBackground(() => {
   chrome.commands.onCommand.addListener((command) => {
     void (async () => {
       switch (command) {
         case "toggle-audio":
         case "toggle-camera": {
-          const tabs = await chrome.tabs.query({
-            url: "https://meet.google.com/*",
-          });
-          const tabIDs = tabs.flatMap(({ id }) =>
-            id === undefined ? [] : [id],
-          );
-
-          if (tabIDs.length === 0) {
-            return;
-          }
-
-          if (tabIDs.length > 1) {
-            chrome.notifications.create({
-              title: chrome.runtime.getManifest().name,
-              message:
-                "Error: Multiple tabs of Google Meet exist. Please close unnecessary ones and keep one.",
-              type: "basic",
-              iconUrl: "icon/128.png",
-            });
-
-            return;
-          }
-
-          const [targetTabID] = tabIDs;
+          const targetTabID = await findMeetTab();
+          if (!targetTabID) return;
 
           void chrome.tabs.sendMessage(targetTabID, {
             type: command === "toggle-audio" ? "toggleAudio" : "toggleCamera",
           });
+          break;
+        }
+        case "activate-meet-tab": {
+          const targetTabID = await findMeetTab();
+          if (!targetTabID) return;
+
+          void chrome.tabs.update(targetTabID, { active: true });
+
           break;
         }
         default: {
